530ba3aeb751ac37762983ac09664b9b04fbf98d
PhysicsTeacher13 <physicsteacher13@gmail.com>
2019-02-26 13:44:56 -0500

    files updated to match requirements

--- motion_planning.py
+++ motion_planning.py
@@ -4,8 +4,10 @@ import msgpack
 from enum import Enum, auto
 
 import numpy as np
+import csv
 
-from planning_utils import a_star, heuristic, create_grid
+
+from planning_utils import a_star, heuristic, create_grid, prune_path, adjust_bearing, read_home
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
@@ -34,6 +36,7 @@ class MotionPlanning(Drone):
 
         # initial state
         self.flight_state = States.MANUAL
+        self.goal_global_position = goal_global_position # added to get global position from drone
 
         # register all your callbacks here
         self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
@@ -42,6 +45,9 @@ class MotionPlanning(Drone):
 
     def local_position_callback(self):
         if self.flight_state == States.TAKEOFF:
+            print('Local Position callback for takeoff !')
+            # print('DEBUG: self.local_position[2]: ', self.local_position[2])
+            # print('DEBUG: self.target_position[2]: ', self.target_position[2])
             if -1.0 * self.local_position[2] > 0.95 * self.target_position[2]:
                 self.waypoint_transition()
         elif self.flight_state == States.WAYPOINT:
@@ -80,11 +86,13 @@ class MotionPlanning(Drone):
     def takeoff_transition(self):
         self.flight_state = States.TAKEOFF
         print("takeoff transition")
+        print('DEBUG: target_position[2]: '.format(self.target_position[2]))
         self.takeoff(self.target_position[2])
 
     def waypoint_transition(self):
         self.flight_state = States.WAYPOINT
         print("waypoint transition")
+        print('DEBUG Waypoint: {0}'.format(self.waypoints)) # check waypoint to see if empty
         self.target_position = self.waypoints.pop(0)
         print('target position', self.target_position)
         self.cmd_position(self.target_position[0], self.target_position[1], self.target_position[2], self.target_position[3])
@@ -120,40 +128,68 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
+        colliders_file = 'colliders.csv'
         
-        # TODO: set home position to (lon0, lat0, 0)
-
+        lat0, lon0 = read_home(colliders_file)
+        print(f'Home lat: {lat0}, lon: {lon0}')
+            
+        # TODO: set home position to (lon0, lat0, 0) from udacidrone api
+        self.set_home_position = (lon0, lat0, 0)
+        print('DEBUG drone home lon0: {0}   lat0: {1}, alt: 0.0'.format(lon0, lat0))
         # TODO: retrieve current global position
- 
+        local_north, local_east, local_down = global_to_local(self.global_position, self.global_home)
+        print(f'Local values  North: {local_north}, East: {local_east}, Down: {local_down}')
+        
         # TODO: convert to current local position using global_to_local()
         
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
         # Read in obstacle map
-        data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
+        data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=3)
         
         # Define a grid for a particular altitude and safety margin around obstacles
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
+        grid_start_north = int(np.ceil(local_north - north_offset))
+        grid_start_east = int(np.ceil(local_east - east_offset))
+        print('Grid start N: {0}  E: {1}'.format(grid_start_north, grid_start_east))
+        grid_start = (grid_start_north, grid_start_east)
         # TODO: convert start position to current position rather than map center
+        # grid_start = ((start_north + -north_offset), (start_east + -east_offset))
         
         # Set goal as some arbitrary position on the grid
-        grid_goal = (-north_offset + 10, -east_offset + 10)
-        # TODO: adapt to set goal as latitude / longitude position and convert
+        #grid_goal = (grid_start[0] + 10, grid_start[1] + 10)
+        goal_north, goal_east, goal_alt = global_to_local(self.goal_global_position, self.global_home)
+        grid_goal = int(np.ceil(goal_north - north_offset)), int(np.ceil(goal_east - east_offset))
+        
+        # TODO: adapt to set goal as latitude / longitude position and convert converted above 
 
         # Run A* to find a path from start to goal
+        """
+        NOTE: if simulator times out - it may need to be restarted
+        """
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
+        print(path)
         # TODO: prune path to minimize number of waypoints
+        print("pruning the path")
+        path = prune_path(path)
+        print('DEBUG: after pruning the path:')
+        print(path)
         # TODO (if you're feeling ambitious): Try a different approach altogether!
-
+        
         # Convert path to waypoints
-        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
+        waypoints = [[int(p[0] + north_offset), int(p[1] + east_offset), TARGET_ALTITUDE, 0] for p in path]
         # Set self.waypoints
+        print('show 1st waypoint: ', waypoints[0])
+
+        # add bearing to waypoints 
+        waypoints = adjust_bearing(waypoints)
+        print('waypoints with bearing: ', waypoints[1])
+        
         self.waypoints = waypoints
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
         self.send_waypoints()
@@ -175,9 +211,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=5760, help='Port number')
     parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    parser.add_argument('--goal_lon', type=str, default='-122.39755', help="Goal longitude")
+    parser.add_argument('--goal_lat', type=str, default='37.79258', help="Goal latitude")
+    parser.add_argument('--goal_alt', type=str, default='0', help ="Goal altitude")
     args = parser.parse_args()
 
     conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
+    goal_global_position = np.fromstring(f'{args.goal_lon},{args.goal_lat},{args.goal_alt}',dtype='Float64', sep=',')
     drone = MotionPlanning(conn)
     time.sleep(1)
 
--- planning_utils.py
+++ planning_utils.py
@@ -1,7 +1,10 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
+import re
+from math import degrees, atan2
 
+# code added from exercises in class
 
 def create_grid(data, drone_altitude, safety_distance):
     """
@@ -20,8 +23,8 @@ def create_grid(data, drone_altitude, safety_distance):
 
     # given the minimum and maximum coordinates we can
     # calculate the size of the grid.
-    north_size = int(np.ceil(north_max - north_min))
-    east_size = int(np.ceil(east_max - east_min))
+    north_size = int(np.ceil((north_max - north_min + 1)))
+    east_size = int(np.ceil((east_max - east_min + 1)))
 
     # Initialize an empty grid
     grid = np.zeros((north_size, east_size))
@@ -55,6 +58,11 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
+    # Add NW, NE, SW, SE
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -75,7 +83,10 @@ def valid_actions(grid, current_node):
 
     # check if the node is off the grid or
     # it's an obstacle
+    
 
+    # Original Check for off grid or obstacle
+    """
     if x - 1 < 0 or grid[x - 1, y] == 1:
         valid_actions.remove(Action.NORTH)
     if x + 1 > n or grid[x + 1, y] == 1:
@@ -84,12 +95,50 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
-
+    """
+    # diagonals
+    """
+    if x - 1 < 0 or y - 1 < 0 or grid[x -1, y] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
+    if x - 1 < 0 or y + 1 > m or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(Action.NORTH_EAST)
+    if x + 1 > n or y - 1 < 0 or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(Action.SOUTH_WEST)
+    if x + 1 > n or y + 1 > m or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)
+    """
+    # modified based on code from github - reference in the student hub
+    
+    if x + 1 > 0:
+        if grid[x + 1, y] == 1:
+            # remove North since we are at the edge 
+            valid_actions.remove(Action.NORTH)
+        if grid[x - 1, y + 1] == 1:
+            valid_actions.remove(Action.NORTH_EAST)
+        if grid[x - 1, y - 1] == 1:
+            valid_actions.remove(Action.NORTH_WEST)
+    if x + 1 > n:
+        if grid[x + 1, y] == 1:
+            # remove south since we are at the edge
+            valid_actions.remove(Action.SOUTH)
+        if grid[x + 1, y + 1] == 1:
+            valid_actions.remove(Action.SOUTH_EAST)
+        if grid[x + 1, y - 1] == 1:
+            valid_actions.remove(Action.SOUTH_WEST)
+    # already removed the SW, SE, NW, NE
+    if y -1 < 0 or grid[x, y - 1] == 1:
+        valid_actions.remove(Action.WEST)
+    if y + 1 > m or grid[x, y + 1] == 1:
+        valid_actions.remove(Action.EAST)
+        
     return valid_actions
 
 
 def a_star(grid, h, start, goal):
-
+    # find lowest cost to goal
+    print('DEBUG: IN the A star function')
+    print('Start value: {0}'.format(start))
+    print('Goal value: {0}'.format(goal))
     path = []
     path_cost = 0
     queue = PriorityQueue()
@@ -101,6 +150,7 @@ def a_star(grid, h, start, goal):
     
     while not queue.empty():
         item = queue.get()
+        current_cost = item[0]
         current_node = item[1]
         if current_node == start:
             current_cost = 0.0
@@ -117,16 +167,18 @@ def a_star(grid, h, start, goal):
                 da = action.delta
                 next_node = (current_node[0] + da[0], current_node[1] + da[1])
                 branch_cost = current_cost + action.cost
-                queue_cost = branch_cost + h(next_node, goal)
+                new_cost = branch_cost + h(next_node, goal)
                 
                 if next_node not in visited:                
                     visited.add(next_node)               
-                    branch[next_node] = (branch_cost, current_node, action)
-                    queue.put((queue_cost, next_node))
+                    queue.put((new_cost, next_node))
+                    
+                    branch[next_node] = (new_cost, current_node, action)
              
     if found:
         # retrace steps
         n = goal
+        print('DEBUG: n value: {0}'.format(n))
         path_cost = branch[n][0]
         path.append(goal)
         while branch[n][1] != start:
@@ -142,5 +194,62 @@ def a_star(grid, h, start, goal):
 
 
 def heuristic(position, goal_position):
+    # return sq root of position[0] - goal position[0] squared + position[1] - goal postition squared
     return np.linalg.norm(np.array(position) - np.array(goal_position))
+
+def read_home(filename):
+    with open(filename) as f:
+        first_line = f.readline()
+    match = re.match(r'^lat0 (.*), lon0 (.*)$', first_line)
+    if match:
+        lat = match.group(1)
+        lon = match.group(2)
+    return np.fromstring(f'{lat},{lon}',dtype='Float64', sep=',')
+
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6):
+    m = np.concatenate((p1, p2, p3), 0)
+    det = np.linalg.det(m)
+    return abs(det) < epsilon
+
+def point(p):
+    return np.array([p[0], p[1], 1.0]).reshape(1, -1)
+
+def prune_path(path): # modified from the class notes
+    pruned_path = [p for p in path]
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i + 1])
+        p3 = point(pruned_path[i + 2])
+
+        if collinearity_check(p1, p2, p3):
+            pruned_path.remove(pruned_path[i + 1])
+        else:
+            i += 1
+    return pruned_path
+
+
+def closest_point(graph, current_point):
+    cp = None
+    dist = 100000
+    for p in graph.nodes:
+        d = np.linalg.norm(np.array(p) - np.array(current_point))
+        if d < dist:
+            cp = p
+            dist = d
+    return cp
+
+
+def adjust_bearing(waypoints):
+    for idx in range(len(waypoints)):
+        if idx > 0:
+            previous_waypoint = waypoints[idx - 1]
+            current_waypoint = waypoints[idx]
+            current_waypoint[3] = np.arctan2((current_waypoint[1] - previous_waypoint[0]), (current_waypoint[0] - previous_waypoint[0]))
+    return waypoints
+    
+
+
+
 
